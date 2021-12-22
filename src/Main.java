fbba0afc43b67de9999ec36c3f3713629f73a985
Tim Sakhaulov <timur.sakhaulov@gmail.com>
2021-12-23 00:25:30 +0500

    fix idk

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,12 @@ public class Main {
         animals[1] = new Dog("Бобик", 500, 10);
         animals[2] = new Cat("Вася", 200, 0);
 
-        for (int i = 0; i < animals.length; i++) {
-            System.out.println("\n"+animals[i].run(200)+"\n");
-            System.out.println(animals[i].run(300)+"\n");
-            System.out.println(animals[i].run(600)+"\n");
-            System.out.println(animals[i].swim(10)+"\n");
-            System.out.println(animals[i].swim(20));
+        for (Animal animal : animals) {
+            System.out.println("\n" + animal.run(200) + "\n");
+            System.out.println(animal.run(300) + "\n");
+            System.out.println(animal.run(600) + "\n");
+            System.out.println(animal.swim(10) + "\n");
+            System.out.println(animal.swim(20));
         }
 
     }
